3e73f10bfe4dcdf6815f30f29483c7e5cebf0b05
Zhaii43 <hanzprahinog@gmail.com>
2026-04-06 11:38:47 +0800

    ws

---
+++ user/migrations/0019_preregistrationotp_token_nullable.py
@@ -0,0 +1,16 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('user', '0018_user_declaration_verified'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='preregistrationotp',
+            name='token',
+            field=models.CharField(blank=True, max_length=64, null=True, unique=True),
+        ),
+    ]
